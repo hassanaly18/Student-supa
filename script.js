9871dd526ff736ae65027c7dcc5bf5753ba6b067
Hassan Ali <hassansiddiqui03024053707@gmail.com>
2026-01-14 23:51:36 +0500

    auth done

--- script.js
+++ script.js
@@ -1,60 +1,72 @@
 // import { createClient } from '@supabase/supabase-js'
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL="https://onixouwmxbojaqkcdttw.supabase.co"
-const SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uaXhvdXdteGJvamFxa2NkdHR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3MTI2NjUsImV4cCI6MjA4MzI4ODY2NX0.UYE8wqNK6llyqA3keElLUAbNYnGr3pKGMl_eJAQ08Nk"
+const SUPABASE_URL = "https://onixouwmxbojaqkcdttw.supabase.co";
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uaXhvdXdteGJvamFxa2NkdHR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3MTI2NjUsImV4cCI6MjA4MzI4ODY2NX0.UYE8wqNK6llyqA3keElLUAbNYnGr3pKGMl_eJAQ08Nk";
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-const form = document.getElementById("studForm")
-const message = document.getElementById("message")
-const table = document.getElementById("stud_table")
+const form = document.getElementById("studForm");
+const message = document.getElementById("message");
+const table = document.getElementById("stud_table");
 
-form.addEventListener("submit", async function(e) {
-    e.preventDefault()
+if (form) {
+  checkAuth();
+
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault();
 
     const studentName = document.getElementById("name").value;
-    const grade = document.getElementById("grade").value
-    const dob = document.getElementById("dob").value
+    const grade = document.getElementById("grade").value;
+    const dob = document.getElementById("dob").value;
 
     //uploading to supabase
-    const {error} = await supabase.from("students").insert([{
+    const { error } = await supabase.from("students").insert([
+      {
         stud_name: studentName,
         grade: grade,
-        dob: dob
-    }])
+        dob: dob,
+      },
+    ]);
 
-    if(error){
-        message.textContent = "Error uploading data"
+    if (error) {
+      message.textContent = "Error uploading data";
+    } else {
+      message.textContent = "Data Saved Successfully";
     }
-    else{
-        message.textContent = "Data Saved Successfully"
-    }
-    window.addEventListener("DOMContentLoaded", loadStudents)
+    window.addEventListener("DOMContentLoaded", loadStudents);
+  });
+}
 
-})
+window.loadStudents = async function () {
+  const { data, err } = await supabase.from("students").select("*");
+  if (err) {
+    alert(err.message);
+    return;
+  }
 
-window.loadStudents = async function() {
-    const {data, err} = await supabase.from("students").select("*")
-    if(err){
-        alert(err.message)
-        return
-    }
-
-    data.forEach(element => {
-        const row = `
+  data.forEach((element) => {
+    const row = `
         <tr>
         <td>${element.stud_name}</td>
         <td>${new Date(element.dob).toLocaleDateString()}</td>
         <td>${element.grade}</td>
-        </tr>`
+        </tr>`;
 
-        table.innerHTML += row
-    });
+    table.innerHTML += row;
+  });
+};
+
+window.addEventListener("DOMContentLoaded", loadStudents);
+
+async function checkAuth() {
+  const { data } = await supabase.auth.getUser();
+
+  if (!data.user) {
+    window.location.href = "login.html";
+  }
 }
-
-window.addEventListener("DOMContentLoaded", loadStudents)
-
 
 //  window.login = async function() {
 //     const email = document.getElementById("email").value
